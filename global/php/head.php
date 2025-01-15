47c4c9c8dad638125656d60734b12c28c8906fba
weu-souza <weuller1561@gmail.com>
2025-01-15 16:44:20 -0300

    feat: home leout ended

--- global/php/head.php
+++ global/php/head.php
@@ -9,6 +9,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="css/style.css">
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- pages/home/index.php
+++ pages/home/index.php
@@ -3,18 +3,69 @@
 
 <head>
     <?php include '../../global/php/head.php' ?>
+    <link rel="stylesheet" href="./css/style.css">
     <title>Home</title>
-    
-</head>
 
+</head>
 <body>
 
-<div class="container">
-    <?php include '../../global/header/Header.php' ?>
-    <Main class="Main-container">
-        <span>Main</span>
-    </Main>
-</div>
+    <div class="container">
+        <?php include '../../global/header/Header.php' ?>
+        <Main class="Main-container">
+            <section class="card-container">
+
+                <div class="card">
+                    <div class="img-home-container">
+                    <img src="/assets/img/noImage.png" alt="imagem da materia escolhida">
+                    </div>
+                    <h3 class="subtitle">Nome materia</h3>
+                    <p class="paragraph">nome professor</p>
+                    <button class="subtitle">Agendar</button>
+                </div>
+                <div class="card">
+                    <div class="img-home-container">
+                    <img src="/assets/img/noImage.png" alt="imagem da materia escolhida">
+                    </div>
+                    <h3 class="subtitle">Nome materia</h3>
+                    <p class="paragraph">nome professor</p>
+                    <button class="subtitle">Agendar</button>
+                </div>
+                <div class="card">
+                    <div class="img-home-container">
+                    <img src="/assets/img/noImage.png" alt="imagem da materia escolhida">
+                    </div>
+                    <h3 class="subtitle">Nome materia</h3>
+                    <p class="paragraph">nome professor</p>
+                    <button class="subtitle">Agendar</button>
+                </div>
+                <div class="card">
+                    <div class="img-home-container">
+                    <img src="/assets/img/noImage.png" alt="imagem da materia escolhida">
+                    </div>
+                    <h3 class="subtitle">Nome materia</h3>
+                    <p class="paragraph">nome professor</p>
+                    <button class="subtitle">Agendar</button>
+                </div>
+                <div class="card">
+                    <div class="img-home-container">
+                    <img src="/assets/img/noImage.png" alt="imagem da materia escolhida">
+                    </div>
+                    <h3 class="subtitle">Nome materia</h3>
+                    <p class="paragraph">nome professor</p>
+                    <button class="subtitle">Agendar</button>
+                </div>
+                <div class="card">
+                    <div class="img-home-container">
+                    <img src="/assets/img/noImage.png" alt="imagem da materia escolhida">
+                    </div>
+                    <h3 class="subtitle">Nome materia</h3>
+                    <p class="paragraph">nome professor</p>
+                    <button class="subtitle">Agendar</button>
+                </div>
+        
+            </section>
+        </Main>
+    </div>
 
 </body>
 
