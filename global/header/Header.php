d503f7b26912871a99a860f1c636eb2e62381d68
weu-souza <weuller1561@gmail.com>
2025-03-24 11:15:35 -0300

    feat: starting mediador page

--- global/header/Header.php
+++ global/header/Header.php
@@ -51,6 +51,7 @@ if (isset($_SESSION['tipo_vinculo'])) {
             <li><a href="/pages/home" class="subtitle">Home</a></li>
             <li><a href="/pages/perfil" class="subtitle">Perfil</a></li>
             <li><a href="/pages/cad-professor" class="subtitle">Cadastro</a></li>
+            <li><a href="/pages/mediador" class="subtitle">Mediador</a></li>
         </ul>
 
         <div class="button-exit-container">
---
+++ pages/mediador/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../../global/php/head.php' ?>
+
+    <title>Mediador</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+    <div class="container">
+
+
+        <?php include '../../global/header/Header.php' ?>
+
+
+        <Main class="Main-container">
+            teste
+
+        </Main>
+    </div>
+</body>
+
+</html>
